b1ee072439c1db1cd3f01ffdaf948b6fe49adccf
Pavel Beshkov <pavel.beshkov@gmail.com>
2013-04-28 17:18:25 +0300

    Added new projects

---
+++ C# Part2/Exam/2013 4 Feb - Morning/4 Feb Morning/ConvertForJS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ConvertForJS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> test = new List<string>();
+            string line;
+            line = Console.ReadLine();
+            while (line != "")
+            {
+                test.Add(line);
+                line = Console.ReadLine();
+            }
+            StreamWriter writer;
+            if (true)
+            {
+                int a = 5;
+            }
+            int a = 10;
+            using (writer = new StreamWriter("C:\\Users\\Pavel\\Desktop\\Test.txt",false,Encoding.UTF8))
+            {
+                foreach (var item in test)
+                {
+                    writer.WriteLine("'"+item+"',");
+                }
+            }
+        }
+    }
+}
